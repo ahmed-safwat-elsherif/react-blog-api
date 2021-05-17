cd733969990a1564144c2c3d63294109bca607d5
aelsherif <s-aelsherif500@zewailcity.edu.eg>
2021-05-17 16:25:58 +0200

    updates in  comments

--- routes/blogs.js
+++ routes/blogs.js
@@ -239,7 +239,7 @@ COMMENTS: {
           populate: { path: "comments.userId" },
         })
         .populate({ path: "comments.userId", select: "-password -blogs" });
-      console.log("new update(comment added):--->", blog);
+      console.log("Success updated)");
       res
         .status(200)
         .send({ success: true, message: "Comment added successfully", blog });
